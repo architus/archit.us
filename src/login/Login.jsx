0fe139dcd2b8e88996ead99a1ea216f0eca3464d
jazevedo6 <jazevedo6@gatech.edu>
2019-06-19 21:08:46 -0400

    add oauth URL to react for now

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import Icon from "../components/Icon/Icon"
+import Icon from "../components/Icon/Icon";
+
+const oauthUrl = ({ clientId }) =>
+  `https://discordapp.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=https%3A%2F%2Faut-bot.com&response_type=code&scope=identify`;
 
 function Login() {
   return (
     <div>
       <div className="container py-5">
         <h1>Login</h1>
-        <a className="btn btn-discord" href="https://api.aut-bot.com/login">
+        <a
+          className="btn btn-discord"
+          href={oauthUrl({ clientId: "475497380360224780" })}
+        >
           <Icon name="discord" className="mr-2" />
           Connect with Discord
         </a>
