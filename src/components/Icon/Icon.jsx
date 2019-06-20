28bc6afb644627ccaa11be888a3cfdeafece7c1e
jazevedo6 <jazevedo6@gatech.edu>
2019-06-19 20:42:43 -0400

    Added login button to Login route

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+// import {
+// } from '@fortawesome/free-solid-svg-icons'
+library.add(faDiscord);
+const typeResolutionMap = {
+  fab: [
+    "discord"
+  ]
+};
+
+const baseStyle = { display: "inline-block", height: "1em", width: "1em" };
+const resolveTypeClass = name => {
+  let foundClass = 'fas' // default
+  for (var typeClass in typeResolutionMap) {
+    if (typeResolutionMap[typeClass].includes(name)) {
+      foundClass = typeClass
+      break
+    }
+  }
+  return foundClass
+};
+const resolveIcon = name => {
+  return [resolveTypeClass(name), name]
+}
+
+const Icon = ({ className, name }) => {
+  return (
+    <span className={className} style={baseStyle}>
+      <FontAwesomeIcon icon={resolveIcon(name)} />
+    </span>
+  );
+};
+
+export default Icon;
