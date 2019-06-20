5dcaecad21e0877fa2200ec77d3a485d26475004
jazevedo6 <jazevedo6@gatech.edu>
2019-06-19 21:15:47 -0400

    Commit to force branch deploy?

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -3,6 +3,7 @@ import Icon from "../components/Icon/Icon";
 
 const oauthUrl = ({ clientId }) =>
   `https://discordapp.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=https%3A%2F%2Faut-bot.com&response_type=code&scope=identify`;
+const autBotClientId = '475497380360224780'
 
 function Login() {
   return (
@@ -11,7 +12,7 @@ function Login() {
         <h1>Login</h1>
         <a
           className="btn btn-discord"
-          href={oauthUrl({ clientId: "475497380360224780" })}
+          href={oauthUrl({ clientId: autBotClientId })}
         >
           <Icon name="discord" className="mr-2" />
           Connect with Discord
