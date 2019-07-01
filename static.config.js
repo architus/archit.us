996223ed3703b24efe7399509384ebc4ca0c570e
jazevedo6 <jazevedo6@gatech.edu>
2019-07-01 17:55:34 -0400

    Fix absolute paths on non-production

--- static.config.js
+++ static.config.js
@@ -1,31 +1,30 @@
-export default {
-  getRoutes: async () => [
-    {
-      path: "/",
-      template: "src/pages/Index"
-    },
-    {
-      path: "login",
-      template: "src/pages/Login"
-    },
-    {
-      path: "404",
-      template: "src/pages/NotFound"
-    },
-    {
-      path: "app",
-      template: "src/dynamic/AppRoot"
-    }
-  ],
+export default Object.assign(
+  {},
+  {
+    getRoutes: async () => [
+      {
+        path: "/",
+        template: "src/pages/Index"
+      },
+      {
+        path: "login",
+        template: "src/pages/Login"
+      },
+      {
+        path: "404",
+        template: "src/pages/NotFound"
+      },
+      {
+        path: "app",
+        template: "src/dynamic/AppRoot"
+      }
+    ],
 
-  plugins: [
-    require.resolve("react-static-plugin-reach-router"),
-    require.resolve("react-static-plugin-sitemap"),
-    require.resolve("react-static-plugin-sass")
-  ],
-
-  siteRoot:
-    process.env.PRODUCTION_URL ||
-    process.env.DEPLOY_PRIME_URL ||
-    "http://localhost:3000"
-};
+    plugins: [
+      require.resolve("react-static-plugin-reach-router"),
+      require.resolve("react-static-plugin-sitemap"),
+      require.resolve("react-static-plugin-sass")
+    ]
+  },
+  process.env.PRODUCTION_URL ? { siteRoot: process.env.PRODUCTION_URL } : {}
+);
