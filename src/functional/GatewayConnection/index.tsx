b1b041bd28ce45c29066558d72bf27daf559abbb
jazevedo6 <jazevedo6@gatech.edu>
2019-12-09 03:31:03 -0500

    Install socket.io

---
+++ src/functional/GatewayConnection/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { websocketConnect } from "store/actions";
+
+export const GatewayConnection: React.FunctionComponent = () => {
+
+}
