824f252af214b456017f61872f5087bf69b0fba2
Joseph Azevedo <joseph.az@gatech.edu>
2020-05-05 11:13:32 -0400

    feat: add built tag near logo

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
+import styled, { Box } from "@xstyled/emotion";
 import Links from "Components/Header/Links";
+import preval from "preval.macro";
 import SessionControl from "Components/Header/SessionControl";
-import { Link } from "Components";
+import Link from "Components/AutoLink";
+import Tooltip from "Components/Tooltip";
+import Icon from "Components/Icon";
 import { LinkProps } from "Components/Router";
+import { AnyIconName } from "Components/Icon/loader";
 import LogoSvg from "Assets/logo.inline.svg";
-import { attach } from "Utility";
+import { attach, isEmptyOrNil } from "Utility";
+import { opacity } from "Theme/getters";
 import "./style.scss";
 
 type HeaderProps = {
@@ -46,9 +52,161 @@ const Header: React.FC<HeaderProps> = ({
 type BrandProps = Partial<LinkProps>;
 
 const Brand: React.FC<BrandProps> = (props) => (
-  <Link {...props} className="nav-link brand" to="/">
-    <div dangerouslySetInnerHTML={{ __html: LogoSvg }} />
-  </Link>
+  <Box display="flex" alignItems="center" mr="micro">
+    <Link {...props} className="nav-link brand" to="/">
+      <div dangerouslySetInnerHTML={{ __html: LogoSvg }} />
+    </Link>
+    <BuildMarker />
+  </Box>
 );
+
+const Styled = {
+  TooltipDivider: styled.hr`
+    border-top: 2px solid;
+    border-top-color: ${opacity("light", 0.4)};
+    margin: pico 0;
+  `,
+  VersionPill: styled.div`
+    display: inline-block;
+    margin-top: femto;
+    margin-left: femto;
+    padding: atto pico;
+    border-radius: 4px;
+    border: 2px solid;
+    border-color: secondary;
+    color: light;
+    background-color: ${opacity("secondary", 0.2)};
+    cursor: pointer;
+
+    font-weight: 500;
+    font-size: 13px;
+    letter-spacing: 1px;
+  `,
+  VersionIcon: styled(Icon)`
+    margin-right: nano;
+  `,
+  BuildInfoList: styled.ul`
+    margin-bottom: 0;
+    padding-left: 1rem;
+  `,
+};
+
+const BuildMarker: React.FC = () => {
+  // See https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
+  const isLocal = process.env.BUILD_LOCATION !== "remote";
+  const isProduction = process.env.BUILD_TAG === "prod";
+  const isBranch = process.env.CONTEXT === "branch-deploy";
+  const isDevelop = isBranch && process.env.BRANCH === "develop";
+  const isPullRequest = process.env.PULL_REQUEST === "true";
+  if (isProduction) return null;
+
+  let text: React.ReactNode | null = null;
+  let tooltip: React.ReactNode | null = null;
+  let icon: AnyIconName | null = null;
+  if (isLocal) {
+    text = "LOCAL";
+    tooltip = (
+      <BuildTooltip>
+        To hide the local tag, set the environment variable
+        &lsquo;BUILD_LOCATION&rsquo; to &lsquo;remote&rsquo;
+      </BuildTooltip>
+    );
+  } else if (isDevelop) {
+    text = "CANARY";
+    tooltip = (
+      <BuildTooltip>
+        Test out the upcoming features for archit.us. Builds directly from the
+        &lsquo;develop&rsquo; branch on GitHub
+      </BuildTooltip>
+    );
+  } else if (isPullRequest) {
+    const prId = process.env.REVIEW_ID;
+    icon = "pull-request";
+    text = `#${prId}`;
+    tooltip = <BuildTooltip>Built automatically from PR #{prId}</BuildTooltip>;
+  } else if (isBranch) {
+    const branch = process.env.BRANCH;
+    icon = "branch";
+    text = branch;
+    tooltip = (
+      <BuildTooltip>
+        Built automatically from branch &lsquo;{branch}&rsquo;
+      </BuildTooltip>
+    );
+  } else {
+    // Must be a commit
+    const commitRef = process.env.COMMIT_REF;
+    icon = "commit";
+    text = commitRef?.substring(0, 7);
+    tooltip = <BuildTooltip>{null}</BuildTooltip>;
+  }
+
+  if (text === null) return null;
+  const inner = (
+    <Styled.VersionPill>
+      {icon && <Styled.VersionIcon name={icon} />}
+      {text}
+    </Styled.VersionPill>
+  );
+  if (tooltip === null) return inner;
+  return (
+    <Tooltip
+      id="site-version-tooltip"
+      text={tooltip}
+      bottom
+      padding={6}
+      maxWidth={320}
+    >
+      {inner}
+    </Tooltip>
+  );
+};
+
+type BuildTooltipProps = { children: React.ReactNode };
+
+const BuildTooltip: React.FC<BuildTooltipProps> = ({ children }) => {
+  const buildTimeString: string = preval`module.exports = (new Date()).toISOString()`;
+  const buildTime = new Date(buildTimeString);
+  const dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  let values: Record<string, string | undefined> = {
+    "Build time": `${buildTime.toLocaleDateString(
+      undefined,
+      dateOptions
+    )} at ${buildTime.toLocaleTimeString()}`,
+  };
+  if (process.env.NETLIFY === "true") {
+    values = {
+      ...values,
+      "Build id": process.env.BUILD_ID,
+      Context: process.env.CONTEXT,
+      "GitHub repository": process.env.REPOSITORY_URL,
+      "Commit SHA": process.env.COMMIT_REF,
+      Branch: process.env.BRANCH,
+      Head: process.env.HEAD,
+      "Deploy URL": process.env.DEPLOY_PRIME_URL,
+      "Deploy Id": process.env.DEPLOY_ID,
+    };
+  }
+
+  return (
+    <Box textAlign="left">
+      {children}
+      {children != null && <Styled.TooltipDivider />}
+      <Styled.BuildInfoList>
+        {Object.entries(values).map(([label, value]) => (
+          <li key={label}>
+            <strong>{label}:</strong> {isEmptyOrNil(value) ? "~" : value}
+          </li>
+        ))}
+      </Styled.BuildInfoList>
+    </Box>
+  );
+};
 
 export default attach(Header, { Brand });
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -12,9 +12,68 @@ declare module "@xstyled/emotion" {
   import React from "react";
   import { jsx, css } from "emotion";
   import styled from "@emotion/styled";
+  import { Theme } from "Theme/tokens";
 
   export const jsx: typeof jsx;
   export const css: typeof css;
+  export const up: (breakpoint: string, css: string) => string;
+  export const down: (breakpoint: string, css: string) => string;
   export const ThemeProvider: React.ComponentType<{ theme: object }>;
+  export const ColorModeProvider: React.ComponentType<{}>;
+  export const useColorMode: () => [string, (newMode: str) => void];
+  export type Space = number | Exclude<keyof Theme["space"], keyof never[]>;
+  export interface BoxProps {
+    // Spacing props
+    margin?: Space;
+    m?: Space;
+    marginTop?: Space;
+    mt?: Space;
+    marginRight?: Space;
+    mr?: Space;
+    marginBottom?: Space;
+    mb?: Space;
+    marginLeft?: Space;
+    ml?: Space;
+    mx?: Space;
+    my?: Space;
+    padding?: Space;
+    p?: Space;
+    paddingTop?: Space;
+    pt?: Space;
+    paddingRight?: Space;
+    pr?: Space;
+    paddingBottom?: Space;
+    pb?: Space;
+    paddingLeft?: Space;
+    pl?: Space;
+    px?: Space;
+    py?: Space;
+    space?: Space;
+    children?: React.ReactNode;
+    display?:
+      | "block"
+      | "inline-block"
+      | "inline"
+      | "flex"
+      | "grid"
+      | "none"
+      | "inherit"
+      | "initial";
+    textAlign?: "left" | "right" | "center" | "justify" | "initial" | "inherit";
+    alignItems?:
+      | "stretch"
+      | "center"
+      | "flex-start"
+      | "flex-end"
+      | "baseline"
+      | "initial"
+      | "inherit";
+  }
+  export const Box: React.ComponentType<BoxProps>;
+
   export default styled;
+}
+
+declare module "preval.macro" {
+  export default function apply(code: string | TemplateStringsArray): string;
 }
