830afa6782c1b6026539798c3ee816e217c7493a
Joseph Azevedo <joseph.az@gatech.edu>
2020-07-19 08:55:07 +0000

    fix: remove old babel config

--- design/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    ["linaria/babel", { evaluate: true, displayName: true }],
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-react",
-  ],
-  plugins: ["@babel/plugin-transform-modules-commonjs"],
-};
