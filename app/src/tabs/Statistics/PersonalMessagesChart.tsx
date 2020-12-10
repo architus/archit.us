cbe501d3b92beea3a214f23cd5a920d6a603a30d
Jonathan Buchanan <johnyburd8@gmail.com>
2020-12-09 19:10:17 -0500

    eslint sucks

--- app/src/tabs/Statistics/PersonalMessagesChart.tsx
+++ app/src/tabs/Statistics/PersonalMessagesChart.tsx
@@ -1,8 +1,6 @@
-import { styled } from "linaria/react";
 import React from "react";
+import { styled } from "linaria/react";
 import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
-
-import CustomResponsiveContainer from "./CustomResponsiveContainer";
 import { Snowflake, Member, User } from "@app/utility/types";
 import { isDefined } from "@architus/lib/utility";
 
@@ -38,57 +36,61 @@ const Styled = {
       }
     }
 
-    &: first-child {
+    & : first-child {
       &::before {
         background-color: #ba5095;
       }
     }
 
-    &: nth-child(2) {
+    & : nth-child(2) {
       &::before {
-        background-color: #844ea3;
+        background-color: #844EA3;
       }
     }
-  `,
-};
+
+  `
+}
+
 
 export type PersonalMessagesChartProps = {
   currentUser: User;
   totalMessages: number;
   memberCounts: Record<string, number>;
-};
+}
 
-export const PersonalMessagesChart: React.FC<PersonalMessagesChartProps> = React.memo(
-  ({ currentUser, totalMessages, memberCounts }) => {
-    const me = memberCounts[currentUser.id as string];
-    const data = [
-      { name: "me", value: me },
-      { name: "not me", value: totalMessages - me },
-    ];
-    return (
-      <Styled.OuterContainer>
-        <CustomResponsiveContainer>
-          <PieChart>
-            <Pie
-              data={data}
-              cx={"35%"}
-              cy={"50%"}
-              innerRadius={"65%"}
-              outerRadius={"90%"}
-              fill="#844EA3"
-              paddingAngle={5}
-              dataKey="value"
-            >
-              <Cell key="cell-0" fill="#ba5095" strokeWidth={0} />
-              <Cell key="cell-1" fill="#844EA3" strokeWidth={0} />
-            </Pie>
-          </PieChart>
-        </CustomResponsiveContainer>
-        <Styled.LabelContainer>
-          <p>{currentUser.username}</p>
-          <p>other</p>
-        </Styled.LabelContainer>
-      </Styled.OuterContainer>
-    );
-  }
-);
+export const PersonalMessagesChart: React.FC<PersonalMessagesChartProps> = React.memo(({
+  currentUser,
+  totalMessages,
+  memberCounts,
+}) => {
+  const me = memberCounts[currentUser.id as string];
+  const data = [
+    { name: "me", value: me},
+    { name: "not me", value: totalMessages - me}
+  ];
+  return (
+    <Styled.OuterContainer>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            data={data}
+            cx={"35%"}
+            cy={"50%"}
+            innerRadius={"65%"}
+            outerRadius={"90%"}
+            fill="#844EA3"
+            paddingAngle={5}
+            dataKey="value"
+          >
+            <Cell key="cell-0" fill="#ba5095" strokeWidth={0} />
+            <Cell key="cell-1" fill="#844EA3" strokeWidth={0} />
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      <Styled.LabelContainer>
+        <p>{currentUser.username}</p>
+        <p>other</p>
+      </Styled.LabelContainer>
+    </Styled.OuterContainer>
+  )
+})
